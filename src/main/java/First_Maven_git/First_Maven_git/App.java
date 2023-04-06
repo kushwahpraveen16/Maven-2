8004c82bef6ad1e4689da8ca439eac5215164b56
kushwahpraveen16 <48704951+kushwahpraveen16@users.noreply.github.com>
2023-04-06 19:38:35 +0530

    first Commit

---
+++ src/main/java/First_Maven_git/First_Maven_git/App.java
@@ -0,0 +1,13 @@
+package First_Maven_git.First_Maven_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/First_Maven_git/First_Maven_git/AppTest.java
@@ -0,0 +1,20 @@
+package First_Maven_git.First_Maven_git;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
